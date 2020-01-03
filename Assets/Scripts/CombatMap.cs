c6b83b74b1fff6f6e0338c082a5122546c64d9a7
wennx <wennx.96@163.com>
2020-01-03 13:54:29 -0500

    Grid Overlay on Combatmap complete

--- Assets/Scripts/CombatMap.cs
+++ Assets/Scripts/CombatMap.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class CombatMap : MonoBehaviour
 {
+    private GameObject GridPlane;
+
+    private Vector2 GridSize;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        GridPlane = transform.Find("GridPlane").gameObject;
+        GridPlane.GetComponent<Renderer>().material.mainTextureScale = new Vector2(8, 10);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void LoadMap()
+    {
+
     }
 }
